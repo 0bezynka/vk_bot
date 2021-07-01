ef839eb60a82cfff36bfaceaafd8ae9252edfa8f
0bezynka <41165411+0bezynka@users.noreply.github.com>
2021-07-01 17:07:46 +0300

    Update nbot.py

--- nbot.py
+++ nbot.py
@@ -1,37 +1,31 @@
 import vk_api
 import time
-import datetime
-now = datetime.datetime.now()
+# Cannot add this user to friends as user not found
+# нужно сделать что бы удалённых пользователей игнорил в подписчиках
 vk=vk_api.VkApi(token="TOKKEN")
 
-#Подписчики
+# Подписчики
 friends_get_Followers = vk.method("users.getFollowers")
-friends_get_Followers_items = (friends_get_Followers["items"])#Spisok 1
+friends_get_Followers_items = (friends_get_Followers["items"])# Spisok 1
 
-#Заявки в др
+# Заявки в др
 friends_get_Requests = vk.method("friends.getRequests")
-list_Requests = (friends_get_Requests["items"])#Spisok 2
+list_Requests = (friends_get_Requests["items"])# Spisok 2
 
-#обьединение списка
+# Обьединение списка
 list_id = friends_get_Followers_items + list_Requests
 
-log_info = ("[ Подписчиков: {}\n[ Заявок в др: {}\n[ Всего ID: {}".format(friends_get_Followers["count"],friends_get_Requests["count"],len(list_id)))
-print(log_info)
-
-# log file ===================================
-log_write = ("{}\nПодписчиков: {} Заявок в др: {} Всего ID: {}\n".format(now.strftime("%d-%m-%Y %H:%M"),friends_get_Followers["count"],friends_get_Requests["count"],len(list_id)))
-log_file = open("log.txt", "a", encoding='utf-8')
-log_file.write(log_write)
-log_file.close()
+info = ("[ Подписчиков: {}\n[ Заявок в др: {}\n[ Всего ID: {}".format(friends_get_Followers["count"],friends_get_Requests["count"],len(list_id)))
+print(info)
 
 #добавление всех айди
-for list_id in list_id:
-    add_friend = vk.method("friends.add", {"user_id":list_id})
-    print ("[+]Заявка одобрена")# .format(schet)
+for add_id in list_id:
+    add_friend = vk.method("friends.add", {"user_id":add_id})
+    print ("[+]Заявка одобрена")
     time.sleep(5)#Одобряет раз в 5 секунды
 
 print("[ Тайм-аут ]")
-time.sleep(60)#Sleep 1 ========================================
+time.sleep(60)# Sleep 1
 
 #Сколько общих друзей вк
 friends_get_Suggestions = vk.method("friends.getSuggestions",{"filter":"mutual","count":42})#Сколько общих друзей вк
@@ -43,20 +37,12 @@ for users_list_id in users_list_id:
     users_first_name = (users_list_id["first_name"])#имя пользователя
     users_last_name = (users_list_id["last_name"])#фамилия пользователей
 
-    user_id_add = vk.method("friends.add", {"user_id":user_id})#добавляем в друзья
+    user_id_add = vk.method("friends.add", {"user_id":user_id})# Добавляем в друзья
     print("[+]Заявка отправлена: {} {}".format(users_first_name,users_last_name))
 
-    # log file ===================================
-    log_request = ("[+]Заявка отправлена: {} {}\n".format(users_first_name,users_last_name))
-    log_file = open("log.txt", "a", encoding='utf-8')
-    log_file.write(log_request)
-    log_file.close()
-
-    time.sleep(5)#отправляет раз в 5 секунд
+    time.sleep(5)# Отправляет раз в 5 секунд
 
 print("Все выполнено!")
-#time.sleep(60)
-
 
 #----------------------------------------------------
 # Author:      0bezynka
