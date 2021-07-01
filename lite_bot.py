1bae098b6e7fb897b0ea0026910763be8b603360
0bezynka <41165411+0bezynka@users.noreply.github.com>
2021-07-01 16:49:53 +0300

    Update lite_bot.py

--- lite_bot.py
+++ lite_bot.py
@@ -1,24 +1,27 @@
 import vk_api
 import time
+"""
+(Накрутка Друзей)
+Получение токена: https://vkhost.github.io/
+pip install vk_api
+"""
+vk=vk_api.VkApi(token="ТОКЕН")
 
-vk=vk_api.VkApi(token="TOKKEN")
-
-friends_getOnline_my = vk.method("friends.getOnline",{"count":10, "order":"random"})#1 Список друзей онлайн
+friends_getOnline_my = vk.method("friends.getOnline",{"count":10, "order":"random"})# Список друзей онлайн
 print("[ Список друзей получил")
 
 for list_get_online in friends_getOnline_my:
-    friends_getOnline_list = vk.method("friends.getOnline",{"user_id":list_get_online,"count":4})#2 Получаем друзей из списка
+    friends_getOnline_list = vk.method("friends.getOnline",{"user_id":list_get_online,"count":4})# Получаем друзей из списка
     print("[ Онлайн получил")
 
     for lists in friends_getOnline_list:
-        add_friends = vk.method("friends.add",{"user_id":lists})#3 Отправляет заявку в друзья
+        add_friends = vk.method("friends.add",{"user_id":lists})# Отправляет заявку в друзья
 
-        users_get = vk.method("users.get",{"user_ids":lists})#4 Узнает кому отправил заявку
+        users_get = vk.method("users.get",{"user_ids":lists})# Узнает кому отправил заявку
         print("[+] Заявка отправлена: {} {}".format(users_get[0]["first_name"],users_get[0]["last_name"]))
         time.sleep(5)
 
 print("Все выполнено!")
-
 
 #----------------------------------------------------
 # Author:      0bezynka
